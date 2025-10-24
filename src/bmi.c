140364a2bbb1ed1ccc7ee5af2e19144738529403
Piotr <79946580+motocyklista@users.noreply.github.com>
2025-10-24 21:26:05 +0200

    Update bmi.c

--- src/bmi.c
+++ src/bmi.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(void) {
+    float kg, m, bmi;  
+
+    printf("Podaj wzrost w centymetrach: ");
+   scanf("%f", &m);
+   m = m / 100.0;  // konwersja na metry, potrzebna do wzoru BMI
+    printf("Podaj wage: ");
+    scanf("%f", &kg);
+
+    bmi = kg / (m * m); // wzór do obliczenia BMI
+     
+if (bmi < 18.5) {
+    printf("BMI ≈ %.1f --> niedowaga\n", bmi);
+}
+
+else if (bmi >= 18.5 && bmi <= 24.9) {
+    printf("BMI ≈ %.1f --> prawidłowe\n", bmi);
+}
+
+else if  (bmi >= 25.0 && bmi <= 29.9) {
+    printf("BMI ≈ %.1f --> nadwaga\n", bmi);
+}
+else if (bmi >= 30.0) {
+    printf("BMI ≈ %.1f --> otyłość\n", bmi);
+}
+
+
+    return 0;
+}
