3707ed0d563a2235a8c4ee5672dda3edcbd3772e
Piotr <79946580+motocyklista@users.noreply.github.com>
2025-10-25 00:06:44 +0200

    Update wind classification thresholds in zad1.c

--- src/zad1.c
+++ src/zad1.c
@@ -13,42 +13,42 @@ if (kn < 1) {
     printf("B=0 (Calm) ");
 }
 
-else if (kn >= 1 && kn <= 3) {
+else if (kn >= 1 && kn < 4) {
     printf("B=1 (Light Air)");
 
 }
 
-else if  (kn >= 4.0 && kn <= 6.0) {
+else if  (kn >= 4.0 && kn < 7.0) {
     printf("B=2 (Light Breeze)");
 }
-else if  (kn >= 7.0 && kn <= 10.0) {
+else if  (kn >= 7.0 && kn < 11.0) {
     printf("B=3 (Gentle Breeze)");
 }
 
-else if  (kn >= 11.0 && kn <= 16.0) {
+else if  (kn >= 11.0 && kn < 17.0) {
     printf("B=4 (Moderate Breeze)");
 }
 
-else if  (kn >= 17.0 && kn <= 21.0) {
+else if  (kn >= 17.0 && kn < 22.0) {
     printf("B=5 (Fresh Breeze)");
 }
 
-else if  (kn >= 22.0 && kn <= 27.0) {
+else if  (kn >= 22.0 && kn < 28.0) {
     printf("B=6 (Strong Breeze)");
 }
-else if  (kn >= 28.0 && kn <= 33.0) {
+else if  (kn >= 28.0 && kn < 34.0) {
     printf("B=7 (Near Gale)");
 }
-else if  (kn >= 34.0 && kn <= 40.0) {
+else if  (kn >= 34.0 && kn < 41.0) {
     printf("B=8 (Gale)");
 }
-else if  (kn >= 41.0 && kn <= 47.0) {
+else if  (kn >= 41.0 && kn < 48.0) {
     printf("B=9 (Strong Gale)");
 }
-else if  (kn >= 48.0 && kn <= 55.0) {
+else if  (kn >= 48.0 && kn < 56.0) {
     printf("B=10 (Storm)");
 }
-else if  (kn >= 56.0 && kn <= 63.0) {
+else if  (kn >= 56.0 && kn < 64.0) {
     printf("B=11 (Violent Storm)");
 }
 
